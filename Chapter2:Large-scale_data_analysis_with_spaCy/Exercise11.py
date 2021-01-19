1900da67f2aed3c0053489f50ca4b4b1154ed792
BrandonHT <brandonhdz2707@gmail.com>
2021-01-18 23:51:50 -0600

    Doc and Span classes

--- Chapter2:Large-scale_data_analysis_with_spaCy/Exercise11.py
+++ Chapter2:Large-scale_data_analysis_with_spaCy/Exercise11.py
@@ -1,14 +1,24 @@
+#Doc and Span classes to process manually
 from spacy.lang.en import English
 
 nlp = English()
 
-# Import the Doc class
-from spacy.tokens import Doc
+# Import the Doc and Span classes
+from spacy.tokens import Doc, Span
 
-# Desired text: "spaCy is cool!"
-words = ["spaCy", "is", "cool", "!"]
-spaces = [True, True, False, False]
+words = ["I", "like", "David", "Bowie"]
+spaces = [True, True, True, False]
 
-# Create a Doc from the words and spaces
+# Create a doc from the words and spaces
 doc = Doc(nlp.vocab, words=words, spaces=spaces)
 print(doc.text)
+
+# Create a span for "David Bowie" from the doc and assign it the label "PERSON"
+span = Span(doc, start=2, end=4, label="PERSON")
+print(span.text, span.label_)
+
+# Add the span to the doc's entities
+doc.ents = [span]
+
+# Print entities' text and labels
+print([(ent.text, ent.label_) for ent in doc.ents])
