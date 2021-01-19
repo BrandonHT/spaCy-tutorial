d0b8e1b7a33f247752c2d7f7448e57f75bc86400
BrandonHT <brandonhdz2707@gmail.com>
2021-01-18 20:47:19 -0600

    String to hashes

---
+++ Chapter2:Large-scale_data_analysis_with_spaCy/Exercise10.py
@@ -0,0 +1,25 @@
+#String to hashes
+
+from spacy.lang.en import English
+
+nlp = English()
+doc = nlp("I have a cat")
+
+# Look up the hash for the word "cat"
+cat_hash = nlp.vocab.strings["cat"]
+print(cat_hash)
+
+# Look up the cat_hash to get the string
+cat_string = nlp.vocab.strings[cat_hash]
+print(cat_string)
+
+nlp = English()
+doc = nlp("David Bowie is a PERSON")
+
+# Look up the hash for the string label "PERSON"
+person_hash = nlp.vocab.strings["PERSON"]
+print(person_hash)
+
+# Look up the person_hash to get the string
+person_string = nlp.vocab.strings[person_hash]
+print(person_string)
